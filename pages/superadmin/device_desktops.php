6d822584ba479d1a65371e6c96f1c786c55f9051
mrp-cl <paningbatanronald25@gmail.com>
2026-06-13 00:27:01 +0800

    check mo pre

--- pages/superadmin/device_desktops.php
+++ pages/superadmin/device_desktops.php
@@ -124,6 +124,8 @@ if ($acq_filter === 'lt5') {
     $baseWhere[] = "d.acquisition_date IS NOT NULL AND d.acquisition_date != '0000-00-00' AND d.acquisition_date >= DATE_SUB(CURDATE(), INTERVAL 5 YEAR)";
 } elseif ($acq_filter === 'gt5') {
     $baseWhere[] = "d.acquisition_date IS NOT NULL AND d.acquisition_date != '0000-00-00' AND d.acquisition_date < DATE_SUB(CURDATE(), INTERVAL 5 YEAR)";
+}elseif ($acq_filter === 'none') {
+    $baseWhere[] = "(d.acquisition_date IS NULL OR d.acquisition_date = '' OR d.acquisition_date = '0000-00-00')";
 }
 
 $whereSQL = !empty($baseWhere) ? "WHERE " . implode(" AND ", $baseWhere) : "";
@@ -400,6 +402,7 @@ $exportParams = http_build_query([
                 $acqLabel = 'ACQ Date';
                 if ($acq_filter === 'lt5') $acqLabel = 'Age < 5 Years';
                 elseif ($acq_filter === 'gt5') $acqLabel = 'Age > 5 Years';
+                elseif ($acq_filter === 'none') $acqLabel = 'No ACQ Date';
                 $acqBase = '?search=' . urlencode($search) . '&' . http_build_query([
                     'division'      => $division_filter,
                     'filter_os'     => $os_filter,
@@ -412,6 +415,7 @@ $exportParams = http_build_query([
                     <li><a class="dropdown-item" href="<?= $acqBase ?>">All</a></li>
                     <li><a class="dropdown-item" href="<?= $acqBase ?>&filter_acq=lt5">Less than 5 years </a></li>
                     <li><a class="dropdown-item" href="<?= $acqBase ?>&filter_acq=gt5">More than 5 years </a></li>
+                    <li><a class="dropdown-item" href="<?= $acqBase ?>&filter_acq=none">No Acquisition Date</a></li>
                 </ul>
             </div>
 
